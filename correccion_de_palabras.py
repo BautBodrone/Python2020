4cac633627c5730ff939f61a084f9829f3a63307
unknown <c.javierfranco99@gmail.com>
2020-08-06 10:01:12 -0300

    correcciones

--- correccion_de_palabras.py
+++ correccion_de_palabras.py
@@ -1,13 +1,10 @@
 from pattern.es import verbs, spelling, lexicon, parse, split
 
 def verbosOAdjetivos(palabra,soloVerbosOadjetivos,dificultad,categoria):
-<<<<<<< HEAD
     """verifico si en caso de que la dificultad sea medio devuelva los verbos o adjetivos, o si en el caso de que sea
     la dificultad dificil devuelva la palabra segun la categoria elegida aleatoriamente """
-=======
     """verifico que sea adjetivo o verbo"""
     #print(palabra)
->>>>>>> 78eb081f5d9b592eea79f20f45c9dc22bfcb1a1e
     dato=parse(palabra)
     VD=parse(palabra).split()[0][0][1]
 
@@ -28,10 +25,6 @@ def verbosOAdjetivos(palabra,soloVerbosOadjetivos,dificultad,categoria):
 
 def palabraValida(palabra,dificultad,categoria):
     """devuelve si la palabra es valida segun su dificultad """
-<<<<<<< HEAD
-
-=======
->>>>>>> 78eb081f5d9b592eea79f20f45c9dc22bfcb1a1e
     dificultadMedia={"medio":('JJ','JJR','JJS','VB','VDB','VBG','VBN','VBP','VBZ'),"verbos":('VB','VDB','VBG','VBN','VBP','VBZ'),"adjetivos":('JJ','JJR','JJS')}
     if (not palabra.lower() in verbs):#pregunta si no es un verbo en caso de que no sea verdadero es porque es un verbo
         if (not palabra.lower() in spelling):# pregunta si es una palabra que esta dentro de spelling
