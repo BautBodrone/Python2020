f967801f3b79037afa2c688a212b07b29b48a23b
unknown <c.javierfranco99@gmail.com>
2020-08-06 06:25:13 -0300

    correcciones al muduloIA

--- buscador_palabra.py
+++ buscador_palabra.py
@@ -2,22 +2,22 @@ import itertools as it
 import random
 import correccion_de_palabras as correccion
 
-dic=dict()
-dic['facil']=2
-dic['medio']=3
-dic['dificil']=4
+#dic=dict()
+#dic['facil']=2
+#dic['medio']=3
+#dic['dificil']=4
 def convertirTupla(tupla):  # sacado de geekforgeeks
 
     str = ''.join(tupla)
     return str
 
-def buscar_palabra(atril, dificultad):
+def buscar_palabra(atril, dificultad,categoria):
     largo_palabras = [*range(2, 8)]
     for i in range(2, 8):
         largo = random.choice(largo_palabras)
         for each in it.permutations(atril,largo):
             palabra=convertirTupla(each)
-            if(correccion.palabraValida(palabra,dificultad)):
+            if(correccion.palabraValida(palabra,dificultad,categoria)):
                 return palabra
         largo_palabras.remove(largo)
     return ""
--- correccion_de_palabras.py
+++ correccion_de_palabras.py
@@ -1,6 +1,6 @@
 from pattern.es import verbs, spelling, lexicon, parse, split
 
-def verbosOAdjetivos(palabra,soloVerbosOadjetivos):
+def verbosOAdjetivos(palabra,soloVerbosOadjetivos,dificultad,categoria):
     """verifico que sea adjetivo o verbo"""
     #print(palabra)
     dato=parse(palabra)
@@ -8,15 +8,22 @@ def verbosOAdjetivos(palabra,soloVerbosOadjetivos):
 
     #print(VD)
     #print(dato)
-    if VD in soloVerbosOadjetivos:
+    if dificultad == "medio":
+        if VD in soloVerbosOadjetivos[dificultad]:
+            print("medio")
+            print(VD)
+            print(palabra)
+            return True
+    elif VD in soloVerbosOadjetivos[categoria]:
+        print(categoria)
         print(palabra)
         return True
     else:
         return False
 
-def palabraValida(palabra,dificultad):
+def palabraValida(palabra,dificultad,categoria):
     """devuelve si la palabra es valida segun su dificultad """
-    dificultadMedia=['JJ','JJR','JJS','VB','VDB','VBG','VBN','VBP','VBZ'] # es una lista con como representa pattern los verbos o adjetivos
+    dificultadMedia={"medio":('JJ','JJR','JJS','VB','VDB','VBG','VBN','VBP','VBZ'),"verbos":('VB','VDB','VBG','VBN','VBP','VBZ'),"adjetivos":('JJ','JJR','JJS')}
     if (not palabra.lower() in verbs):#pregunta si no es un verbo en caso de que no sea verdadero es porque es un verbo
         if (not palabra.lower() in spelling):# pregunta si es una palabra que esta dentro de spelling
             if (not(palabra.lower() in lexicon) and not(palabra.upper() in lexicon) and not(palabra.capitalize() in lexicon)):#pregunta si esta dentro de lexicon
@@ -31,4 +38,4 @@ def palabraValida(palabra,dificultad):
         if (dificultad == 'facil'):
             return True
         else:
-            return verbosOAdjetivos(palabra, dificultadMedia)
+            return verbosOAdjetivos(palabra, dificultadMedia,dificultad,categoria)
